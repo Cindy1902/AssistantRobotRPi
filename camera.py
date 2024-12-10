ebf85c79ca69f63320aa4770ea0932784a88cdbf
LeviathanNguyen <levianth74@gmail.com>
2024-12-11 02:26:27 +0700

    Base project

---
+++ camera.py
@@ -0,0 +1,5 @@
+from tensorflow.keras import Sequential
+from tensorflow.keras.models import load_model
+import cv2
+import numpy as np
+from tensorflow.keras.preprocessing.image import img_to_array
